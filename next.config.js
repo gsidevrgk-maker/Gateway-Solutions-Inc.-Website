dde3923bc732c546c0d8229e2bd3b2ebaef7416a
gsidevrgk-maker <gsidevrgk@gmail.com>
2026-08-09 19:22:21 +0530

    config1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    eslint: {
+      // Disables ESLint errors from blocking production builds on Vercel
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
